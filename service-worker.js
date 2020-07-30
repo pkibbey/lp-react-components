feab75101614d023fc783ab9bdda2d9da4eca634
Phineas Kibbey <pkibbey@liveperson.com>
2020-07-30 11:04:44 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.29e5c2031ce0b1a91d6d460daa6b74f0.js"
+  "precache-manifest.9d80018210cd039fa900606217949b64.js"
 );
 
 self.addEventListener('message', (event) => {
